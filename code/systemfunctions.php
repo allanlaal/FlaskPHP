f5af7949cde1cc1a346a7d2ea7010ddbc66be8fb
Indrek Siitan <indrek.siitan@codelab.ee>
2019-04-03 19:47:06 +0300

    Polyfill getallheaders()

--- code/systemfunctions.php
+++ code/systemfunctions.php
@@ -13,6 +13,27 @@
 	 */
 
 
+	/**
+	 *   Polyfill for getallheaders()
+	 *   @return array
+	 */
+
+	if (!function_exists('getallheaders'))
+	{
+		function getallheaders()
+		{
+			$headers=[];
+			foreach ($_SERVER as $name => $value)
+			{
+				if (substr($name,0,5)=='HTTP_')
+				{
+					$headers[str_replace(' ','-',ucwords(strtolower(str_replace('_',' ',substr($name,5)))))]=$value;
+				}
+			}
+			return $headers;
+		}
+	}
+
 
 	/**
 	 *   Return a time with milliseconds
