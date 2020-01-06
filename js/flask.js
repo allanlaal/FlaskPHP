cc9a7dbbef9f2efd5cdfd4688cedc3e17500dbe3
Indrek Siitan <indrek.siitan@codelab.ee>
2020-01-06 13:52:58 +0200

    Fire date field onChange event when picking a date from the calendar

--- js/flask.js
+++ js/flask.js
@@ -513,6 +513,9 @@ Flask.initElements = function( base )
 					if (!date) return '';
 					return moment(date).format(dateFormat);
 				}
+			},
+			onChange: function(date, text) {
+				$('input',this).val(text).trigger('change');
 			}
 		});
 	});
